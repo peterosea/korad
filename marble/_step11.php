cb5abde75947fa355d8ca19965299430bca672ef
porcoRosso <jushan1980@gmail.com>
2020-10-29 18:52:23 +0900

    임시오픈전 수정작업 #9

--- marble/_step11.php
+++ marble/_step11.php
@@ -1,4 +1,7 @@
-<h2 class="_title">동굴처분시설로 이동</h2>
+<h2 class="_title">
+    동굴처분시설로 이동
+    <img src="public/img/marble-image-05.png">
+</h2>
 <p class="_description">
 모든 검사를 통과한 방사성폐기물이 세상과 완전히 격리되는 곳이 동굴처분시설입니다.
 </p>
--- marble/_step12.php
+++ marble/_step12.php
@@ -1,4 +1,7 @@
-<h2 class="_title">여기서 잠깐!<br>동굴처분시설은 안전한가요?</h2>
+<h2 class="_title">
+    여기서 잠깐!<br>동굴처분시설은 안전한가요?
+    <img src="public/img/marble-image-06.png">
+</h2>
 <p class="_description">
 일본 쓰나미가 제일 높았던 사례가 11m입니다.<br>
 동굴입구는 해수면보다 30m 높게 위치하고 있기 때문에 안전합니다.
